df64e19f9f3cbb98e0f78dacd06b69d132cd47c4
szymon <szymon.piotr.duda@gmail.com>
2021-03-23 22:55:07 +0100

    initial commit: frontity

---
+++ frontity.settings.js
@@ -0,0 +1,57 @@
+const settings = {
+  "name": "muzeum",
+  "state": {
+    "frontity": {
+      "url": "http://lucanus.ayz.pl",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "http://lucanus.ayz.pl"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
