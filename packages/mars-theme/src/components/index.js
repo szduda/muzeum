4eba9bb4b9141d02df3affd39e7ce51f734a7273
szymon <szymon.piotr.duda@gmail.com>
2021-04-19 01:54:56 +0200

    add meta wep-app-capable

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -51,6 +51,7 @@ const Theme = ({ state, actions }) => {
       <Title />
       <Head>
         <meta name="description" content={state.frontity.description} />
+        <meta name="mobile-web-app-capable" content="yes" />
         <html lang="en" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
