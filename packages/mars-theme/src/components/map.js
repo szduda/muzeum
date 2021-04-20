29bfabdfb4ed4cb589bbaddcf5753448f9c56018
szymon <szymon.piotr.duda@gmail.com>
2021-04-20 22:58:50 +0200

    update icon buttons style

--- packages/mars-theme/src/components/map.js
+++ packages/mars-theme/src/components/map.js
@@ -32,10 +32,11 @@ const getBodyLockStyle = ({ padRight }) => ({
 
 const MapToggle = styled.button`
   position: fixed;
+  cursor: pointer;
   right: 0.5rem;
   bottom: 1rem;
   padding: 0.5rem;
-  border: 0;
+  border: 2px solid #f9c959;
   background: #f9c959;
   border-radius: 8px;
   z-index: 8;
@@ -45,6 +46,17 @@ const MapToggle = styled.button`
   align-items: center;
   justify-content: center;
   ${({ isOpen, isWideScreen }) => isOpen && `right: calc(0.5rem + ${isWideScreen ? '15px' : 0});`}
+
+  svg {
+    fill: #444;
+  }
+
+  :hover {
+    background: #444;
+    svg {
+      fill: #f9c959;
+    }
+  }
 
   @media (min-width: 768px) {
     right: calc(0.5rem + 15px);
